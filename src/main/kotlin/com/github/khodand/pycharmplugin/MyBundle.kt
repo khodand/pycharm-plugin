a426132701875cc598df9c4d9dafea9602cf6126
GitHub Action <action@github.com>
2020-09-12 13:11:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/khodand/pycharmplugin/MyBundle.kt
+++ src/main/kotlin/com/github/khodand/pycharmplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.khodand.pycharmplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/khodand/pycharmplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/khodand/pycharmplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.khodand.pycharmplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.khodand.pycharmplugin.MyBundle
 
 class MyApplicationService {
 
